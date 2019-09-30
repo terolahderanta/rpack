d99a31f23447294643768eec526ac17432c16c38
terolahderanta <tero.lahderanta@oulu.fi>
2019-09-30 14:18:08 +0300

    Altered random_sigma to generate different types of matrices.

--- R/sim_data.R
+++ R/sim_data.R
@@ -25,11 +25,27 @@ simulate_data <- function(n, w_dist_params = c(1, 100), coord_dist = "uniform", 
   return(sim_data)
 }
 
-random_sigma <- function(){
-  A <- matrix(runif(4, min = 0.1, max = 0.8),ncol = 2, nrow = 2)
-  Sigma <- t(A) %*% A 
-  Sigma[1,1] <- Sigma[1,1] + runif(1)
-  Sigma[2,2] <- Sigma[2,2] + runif(1)
+
+#' Simulates random sigma for the normal and laplace distribution.
+#'
+#' @param no_corr Is there correlation between x and y? 
+#' @return Covariance matrix.
+#' @export
+#'
+#' @examples
+random_sigma <- function(no_corr = TRUE){
+  
+  if(no_corr){
+    r_diag <- runif(1, min = 1, max = 2)
+    Sigma <- diag(x = rep(r_diag, 2))
+    
+  } else {
+    A <- matrix(runif(4, min = 0.1, max = 0.8),ncol = 2, nrow = 2)
+    Sigma <- t(A) %*% A 
+    Sigma[1,1] <- Sigma[1,1] + runif(1)
+    Sigma[2,2] <- Sigma[2,2] + runif(1)
+  }
+  
   return(Sigma)
 }
 
