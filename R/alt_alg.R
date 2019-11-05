d66e3a58a199542cd6fe4e5a669327d4924eea3e
terolahderanta <tero.lahderanta@oulu.fi>
2019-11-05 16:22:03 +0200

    Removed unnecessary options.

--- R/alt_alg.R
+++ R/alt_alg.R
@@ -25,23 +25,16 @@ alt_alg <- function(coords, weights, k, N = 10, range = as.numeric(bounds(weight
                     frac_memb = FALSE, place_to_point = TRUE, fixed_mu = NULL, gurobi_params = NULL,
                     dist_mat = NULL, predet_locations = NULL){
   
-  if(N < 2){
-    N <- 2
-  }
   
-  w <- weights
-  if(is.null(mu_initialization)){
-    init_mu <- NULL
-    
-  } else if(mu_initialization == "random"){
-    init_mu <- list()
-    for (i in 1:N) {
-      init_mu[[i]] <- as.matrix(coords[sample(x = 1:nrow(coords), size = k),])
-    }
-    
-  } else {
-    init_mu <- NULL
-  }
+ #if(!is.null(mu_initialization) & mu_initialization == "random"){
+#   init_mu <- list()
+#   for (i in 1:N) {
+#     init_mu[[i]] <- as.matrix(coords[sample(x = 1:nrow(coords), size = k),])
+#   }
+# } else {
+#   init_mu <- NULL
+# }
+  init_mu <- NULL
   
   # Print the information about run
   cat(paste("Progress bar (N = ", N,"):\n", sep = ""))
@@ -49,28 +42,9 @@ alt_alg <- function(coords, weights, k, N = 10, range = as.numeric(bounds(weight
   
   progress_bar <- 0
   
-  # Call prob_clust function first time
-  temp <- prob_clust(data = coords,
-                     weights = w,
-                     k = k,
-                     prior_dist = "uniform",
-                     range = c(range[1], range[2]),
-                     capacity_weights = capacity_weights,
-                     lambda = lambda,
-                     d = d,
-                     init_mu = init_mu[[1]],
-                     place_to_point = place_to_point,
-                     frac_memb = frac_memb,
-                     fixed_mu = fixed_mu,
-                     gurobi_params = gurobi_params,
-                     dist_mat = dist_mat,
-                     predet_locations = predet_locations)
-  
-  min_obj <- temp$obj
-  best_temp <- temp
-  for (i in 2:N) {
+  for (i in 1:N) {
     temp <- prob_clust(data = coords,
-                       weights = w,
+                       weights = weights,
                        k = k,
                        prior_dist = "uniform",
                        range = c(range[1], range[2]),
@@ -84,6 +58,11 @@ alt_alg <- function(coords, weights, k, N = 10, range = as.numeric(bounds(weight
                        gurobi_params = gurobi_params,
                        dist_mat = dist_mat,
                        predet_locations = predet_locations)
+    
+    if(i == 1){
+      min_obj <- temp$obj
+      best_temp <- temp
+    }
     
     # Print the number of completed laps
     if(floor((i/N)*30) > progress_bar){
